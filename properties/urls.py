e9853c9b83747fb2e4d3c3065651ac9c5e0e2a81
Touseeq Ijaz <112577743+goingtointernet@users.noreply.github.com>
2022-09-01 03:59:21 -0700

    Update urls.py

--- properties/urls.py
+++ properties/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('',views.allproperty, name='allproperty'),
     path('lease',views.alllease, name='alllease'),
-    path('rent',views.alllease, name='allrent'),
+    path('rent',views.allrent, name='allrent'),
     path('<str:slug>',views.propertypost, name='propertypost'),
 ]
